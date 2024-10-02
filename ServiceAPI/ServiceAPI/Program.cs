a87c371120ebe76240ed95e64ca573331e692f68
Larissa Queiroz <larissa.queiroz@nepen.org.br>
2024-10-02 10:21:41 -0300

    verify and apply migrations on program

--- ServiceAPI/ServiceAPI/Program.cs
+++ ServiceAPI/ServiceAPI/Program.cs
@@ -33,6 +33,17 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ServiceAPIContext>();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
